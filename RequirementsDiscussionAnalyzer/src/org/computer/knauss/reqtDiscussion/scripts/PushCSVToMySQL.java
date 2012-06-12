f5e228c6cc7b877d3b5ba03ce9a071b02b4cf8a8
Eric Knauss <eric.knauss@googlemail.com>
2012-06-12 20:55:06 +0200

    some minor change on the configuration

--- RequirementsDiscussionAnalyzer/src/org/computer/knauss/reqtDiscussion/scripts/PushCSVToMySQL.java
+++ RequirementsDiscussionAnalyzer/src/org/computer/knauss/reqtDiscussion/scripts/PushCSVToMySQL.java
@@ -18,8 +18,8 @@ public class PushCSVToMySQL {
 		Properties p = new Properties();
 		p.setProperty(SQLDiscussionEventDAO.PROP_URL,
 				"jdbc:mysql://db.disi.unitn.it/developers");
-		p.setProperty(SQLDiscussionEventDAO.PROP_USER, "ericknauss");
-		p.setProperty(SQLDiscussionEventDAO.PROP_PWD, "ericknauss");
+		p.setProperty(SQLDiscussionEventDAO.PROP_USER, "");
+		p.setProperty(SQLDiscussionEventDAO.PROP_PWD, "");
 		p.setProperty(CSVDiscussionEventDAO.PROP_FILENAME,
 				"testfiles/example-workitemcomments.csv");
 		p.setProperty(CSVDiscussionEventDAO.PROP_ID_COL,
