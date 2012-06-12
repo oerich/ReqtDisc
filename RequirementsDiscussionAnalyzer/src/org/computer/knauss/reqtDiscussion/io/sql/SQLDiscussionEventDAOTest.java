d27aaee0010a84c2efd537c32a34191ac73cb477
Eric Knauss <eric.knauss@googlemail.com>
2012-06-13 00:58:05 +0200

    moved configuration to a file not in github

--- RequirementsDiscussionAnalyzer/src/org/computer/knauss/reqtDiscussion/io/sql/SQLDiscussionEventDAOTest.java
+++ RequirementsDiscussionAnalyzer/src/org/computer/knauss/reqtDiscussion/io/sql/SQLDiscussionEventDAOTest.java
@@ -1,14 +1,15 @@
 package org.computer.knauss.reqtDiscussion.io.sql;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
-import java.sql.Date;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.Properties;
 
 import org.computer.knauss.reqtDiscussion.model.DiscussionEvent;
 import org.junit.Before;
 import org.junit.Test;
-
 
 public class SQLDiscussionEventDAOTest {
 
@@ -20,12 +21,13 @@ public class SQLDiscussionEventDAOTest {
 	}
 
 	@Test
-	public void testGetDiscussionEventsOfDiscussion() throws DAOException {
+	public void testGetDiscussionEventsOfDiscussion() throws DAOException,
+			FileNotFoundException, IOException {
 		Properties p = new Properties();
-		p.setProperty(SQLDiscussionEventDAO.PROP_URL,
-				"jdbc:mysql://db.disi.unitn.it/developers");
-		p.setProperty(SQLDiscussionEventDAO.PROP_USER, "mist");
-		p.setProperty(SQLDiscussionEventDAO.PROP_PWD, "mist");
+		p.load(new FileInputStream("trento-mysql-properties.txt"));
+
+		// System.out.println(p.getProperty(SQLDiscussionEventDAO.PROP_USER));
+
 		this.testDao.configure(p);
 		DiscussionEvent[] events = this.testDao
 				.getDiscussionEventsOfDiscussion(12345);
@@ -37,21 +39,22 @@ public class SQLDiscussionEventDAOTest {
 
 	@Test
 	public void testStoreDiscussionEvents() throws DAOException {
-		DiscussionEvent de = new DiscussionEvent();
-		de.setID(1);
-		de.setDiscussionID(1);
-		de.setContent("TESTTESTTEST");
-		de.setCreationDate(new Date(System.currentTimeMillis()));
-		de.setCreator("JUnit");
 
-		Properties p = new Properties();
-		p.setProperty(SQLDiscussionEventDAO.PROP_URL,
-				"jdbc:mysql://db.disi.unitn.it/developers");
-		p.setProperty(SQLDiscussionEventDAO.PROP_USER, "mist");
-		p.setProperty(SQLDiscussionEventDAO.PROP_PWD, "mist");
-		this.testDao.configure(p);
-
-		this.testDao.storeDiscussionEvents(new DiscussionEvent[] { de });
+		// DiscussionEvent de = new DiscussionEvent();
+		// de.setID(1);
+		// de.setDiscussionID(1);
+		// de.setContent("TESTTESTTEST");
+		// de.setCreationDate(new Date(System.currentTimeMillis()));
+		// de.setCreator("JUnit");
+		//
+		// Properties p = new Properties();
+		// p.setProperty(SQLDiscussionEventDAO.PROP_URL,
+		// "jdbc:mysql://db.disi.unitn.it/developers");
+		// p.setProperty(SQLDiscussionEventDAO.PROP_USER, "mist");
+		// p.setProperty(SQLDiscussionEventDAO.PROP_PWD, "mist");
+		// this.testDao.configure(p);
+		//
+		// this.testDao.storeDiscussionEvents(new DiscussionEvent[] { de });
 	}
 
 }
--- RequirementsDiscussionAnalyzer/src/org/computer/knauss/reqtDiscussion/scripts/PushCSVToMySQL.java
+++ RequirementsDiscussionAnalyzer/src/org/computer/knauss/reqtDiscussion/scripts/PushCSVToMySQL.java
@@ -1,5 +1,8 @@
 package org.computer.knauss.reqtDiscussion.scripts;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.Properties;
 
 import org.computer.knauss.reqtDiscussion.io.csv.CSVDiscussionEventDAO;
@@ -7,40 +10,37 @@ import org.computer.knauss.reqtDiscussion.io.sql.DAOException;
 import org.computer.knauss.reqtDiscussion.io.sql.SQLDiscussionEventDAO;
 import org.computer.knauss.reqtDiscussion.model.DiscussionEvent;
 
-
 public class PushCSVToMySQL {
 
 	/**
 	 * @param args
+	 * @throws IOException
+	 * @throws FileNotFoundException
 	 */
-	public static void main(String[] args) {
+	public static void main(String[] args) throws FileNotFoundException,
+			IOException {
 
 		Properties p = new Properties();
-		p.setProperty(SQLDiscussionEventDAO.PROP_URL,
-				"");
-		p.setProperty(SQLDiscussionEventDAO.PROP_USER, "");
-		p.setProperty(SQLDiscussionEventDAO.PROP_PWD, "");
+		// load url, username and password from a file that is not in github
+		p.load(new FileInputStream("trento-mysql-properties.txt"));
+
 		p.setProperty(CSVDiscussionEventDAO.PROP_FILENAME,
 				"testfiles/example-workitemcomments.csv");
-		p.setProperty(CSVDiscussionEventDAO.PROP_ID_COL,
-				"4");
-		p.setProperty(CSVDiscussionEventDAO.PROP_DISC_ID_COL,
-				"1");
-		p.setProperty(CSVDiscussionEventDAO.PROP_CONTENT_COL,
-				"0");
-		p.setProperty(CSVDiscussionEventDAO.PROP_CDATE_COL,
-				"2");
-		p.setProperty(CSVDiscussionEventDAO.PROP_CREATOR_COL,
-				"3");
+		p.setProperty(CSVDiscussionEventDAO.PROP_ID_COL, "4");
+		p.setProperty(CSVDiscussionEventDAO.PROP_DISC_ID_COL, "1");
+		p.setProperty(CSVDiscussionEventDAO.PROP_CONTENT_COL, "0");
+		p.setProperty(CSVDiscussionEventDAO.PROP_CDATE_COL, "2");
+		p.setProperty(CSVDiscussionEventDAO.PROP_CREATOR_COL, "3");
 		p.setProperty(CSVDiscussionEventDAO.PROP_START_ROW, "1");
-		
+
 		CSVDiscussionEventDAO csvDAO = new CSVDiscussionEventDAO();
 		SQLDiscussionEventDAO sqlDAO = new SQLDiscussionEventDAO();
 		csvDAO.configure(p);
 		sqlDAO.configure(p);
-		
+
 		try {
-			DiscussionEvent[] des = csvDAO.getDiscussionEventsOfDiscussion(32654);
+			DiscussionEvent[] des = csvDAO
+					.getDiscussionEventsOfDiscussion(32654);
 			sqlDAO.storeDiscussionEvents(des);
 			des = csvDAO.getDiscussionEventsOfDiscussion(44088);
 			sqlDAO.storeDiscussionEvents(des);
