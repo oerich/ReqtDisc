71502ec53cfcf290d78015362837cc96c8e60ebf
Eric Knauss <eric.knauss@googlemail.com>
2012-07-19 16:45:49 -0700

    Two more metrics

---
+++ RequirementsDiscussionAnalyzer/src/org/computer/knauss/reqtDiscussion/model/metric/NetworkEdgeCountMetric.java
@@ -0,0 +1,26 @@
+package org.computer.knauss.reqtDiscussion.model.metric;
+
+import org.computer.knauss.reqtDiscussion.model.Discussion;
+import org.computer.knauss.reqtDiscussion.model.socialNetwork.Node;
+
+public class NetworkEdgeCountMetric extends AbstractNetworkMetric {
+
+	@Override
+	public String getName() {
+		return "#Edges";
+	}
+
+	@Override
+	public Double considerDiscussions(Discussion[] wi) {
+		Node[] nodes = getSocialNetwork().getActors();
+
+		double result = 0.0;
+		for (Node n1 : nodes)
+			for (Node n2 : nodes)
+				if (getSocialNetwork().getWeight(n1, n2) > 0)
+					result++;
+
+		return result;
+	}
+
+}
---
+++ RequirementsDiscussionAnalyzer/src/org/computer/knauss/reqtDiscussion/model/metric/PatternMetric.java
@@ -0,0 +1,59 @@
+package org.computer.knauss.reqtDiscussion.model.metric;
+
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+
+import org.computer.knauss.reqtDiscussion.model.Discussion;
+import org.computer.knauss.reqtDiscussion.model.DiscussionEvent;
+import org.computer.knauss.reqtDiscussion.model.clarificationPatterns.BackToDraftPattern;
+import org.computer.knauss.reqtDiscussion.model.clarificationPatterns.ClosingGatePattern;
+import org.computer.knauss.reqtDiscussion.model.clarificationPatterns.HappyEndingPattern;
+import org.computer.knauss.reqtDiscussion.model.clarificationPatterns.IPatternClass;
+import org.computer.knauss.reqtDiscussion.model.clarificationPatterns.IndifferentPattern;
+import org.computer.knauss.reqtDiscussion.model.clarificationPatterns.NoSharedUnderstandingPattern;
+import org.computer.knauss.reqtDiscussion.model.clarificationPatterns.PerfectPattern;
+import org.computer.knauss.reqtDiscussion.model.partition.FixedNumberPartition;
+import org.computer.knauss.reqtDiscussion.model.partition.IDiscussionOverTimePartition;
+
+public class PatternMetric extends AbstractDiscussionMetric {
+
+	private static final IPatternClass[] PATTERNS = new IPatternClass[] {
+			new IndifferentPattern(), new NoSharedUnderstandingPattern(),
+			new ClosingGatePattern(), new BackToDraftPattern(),
+			new HappyEndingPattern(), new PerfectPattern() };
+	private IDiscussionOverTimePartition partition;
+
+	@Override
+	public String getName() {
+		return "Pattern";
+	}
+
+	@Override
+	public Double considerDiscussions(Discussion[] discussions) {
+		for (int i = 0; i < PATTERNS.length; i++) {
+			IPatternClass p = PATTERNS[i];
+			p.setCommentPartition(this.partition);
+			if (p.matchesPattern(getEvents(discussions)))
+				return (double) i;
+		}
+		return -1.0;
+	}
+
+	@Override
+	public void initDiscussions(Discussion[] discussions) {
+		if (this.partition == null) {
+			this.partition = new FixedNumberPartition();
+			this.partition.setPartitionCount(32);
+		}
+		this.partition.setTimeInterval(discussions);
+		this.partition.setDiscussionEvents(getEvents(discussions));
+	}
+
+	private DiscussionEvent[] getEvents(Discussion[] discussions) {
+		List<DiscussionEvent> tmp = new LinkedList<DiscussionEvent>();
+		for (Discussion d : discussions)
+			Collections.addAll(tmp, d.getAllComments());
+		return tmp.toArray(new DiscussionEvent[0]);
+	}
+}
