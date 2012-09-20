cdbb1a6267c0602ebe709977562d6f4a301ea677
Eric Knauss <eric.knauss@googlemail.com>
2012-09-20 14:15:14 -0700

    Added empty NetworkLineConnectivityMetric

---
+++ RequirementsDiscussionAnalyzer/src/main/java/org/computer/knauss/reqtDiscussion/model/metric/NetworkLineConnectivity.java
@@ -0,0 +1,25 @@
+package org.computer.knauss.reqtDiscussion.model.metric;
+
+import org.computer.knauss.reqtDiscussion.model.Discussion;
+
+public class NetworkLineConnectivity extends AbstractDiscussionMetric {
+
+	@Override
+	public String getName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Double considerDiscussions(Discussion[] wi) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int measurementType() {
+		// TODO I have no idea
+		return ORDINAL_TYPE;
+	}
+
+}
