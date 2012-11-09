f4f4d983369521148ab2e14c4e0bd41dfc15cb65
Eric Knauss <eric.knauss@googlemail.com>
2012-11-09 15:15:29 -0800

    the old network centrality metric that uses weights instead of degrees

---
+++ RequirementsDiscussionAnalyzer/src/main/java/org/computer/knauss/reqtDiscussion/model/metric/WeightedNetworkDegreeCentralityMetric.java
@@ -0,0 +1,63 @@
+package org.computer.knauss.reqtDiscussion.model.metric;
+
+import org.computer.knauss.reqtDiscussion.model.Discussion;
+import org.computer.knauss.reqtDiscussion.model.socialNetwork.Node;
+
+public class WeightedNetworkDegreeCentralityMetric extends AbstractNetworkMetric {
+
+	@Override
+	public String getName() {
+		return "Network Centrality";
+	}
+
+	@Override
+	public Double considerDiscussions(Discussion[] d) {
+		if (d.length == 0)
+			return 0.0;
+
+		// initNetwork(wi);
+
+		// compute centrality of each node
+		Node[] nodes = getSocialNetwork().getActors();
+		if (nodes.length <= 2)
+			return 0.0;
+
+		// compute network centrality and remember the max
+		double[] centrality = new double[nodes.length];
+		double maxCentrality = 0;
+		for (int i = 0; i < nodes.length; i++) {
+			centrality[i] = degreeCentrality(nodes[i]);
+			if (centrality[i] > maxCentrality)
+				maxCentrality = centrality[i];
+		}
+
+		double ret = 0;
+		for (int i = 0; i < centrality.length; i++) {
+			ret += maxCentrality - centrality[i];
+		}
+		return ret / ((nodes.length - 1) * (nodes.length - 2));
+	}
+
+	double degreeCentrality(Node node) {
+		Node[] nodes = getSocialNetwork().getActors();
+
+		if (nodes.length < 2)
+			return 0.0;
+		double nodeWeight = 0;
+		for (int j = 0; j < nodes.length; j++) {
+			double weight = getSocialNetwork().getWeight(node, nodes[j])
+					+ getSocialNetwork().getWeight(nodes[j], node);
+			if (weight > getMinWeight())
+				nodeWeight += weight;
+		}
+		double nodeCentrality = nodeWeight / (nodes.length - 1);
+		return nodeCentrality;
+	}
+
+	@Override
+	public int measurementType() {
+		// TODO I have no idea...
+		return ORDINAL_TYPE;
+	}
+
+}
