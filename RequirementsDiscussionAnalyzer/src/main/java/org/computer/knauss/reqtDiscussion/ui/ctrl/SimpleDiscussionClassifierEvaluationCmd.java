92d247df25c07126f5866283ad3d3f5e69146524
Eric Knauss <eric.knauss@googlemail.com>
2012-11-14 17:12:01 -0800

    evaluation almost done

--- RequirementsDiscussionAnalyzer/src/main/java/org/computer/knauss/reqtDiscussion/ui/ctrl/SimpleDiscussionClassifierEvaluationCmd.java
+++ RequirementsDiscussionAnalyzer/src/main/java/org/computer/knauss/reqtDiscussion/ui/ctrl/SimpleDiscussionClassifierEvaluationCmd.java
@@ -9,6 +9,12 @@ import org.computer.knauss.reqtDiscussion.model.Discussion;
 import org.computer.knauss.reqtDiscussion.model.DiscussionEvent;
 import org.computer.knauss.reqtDiscussion.model.DiscussionEventClassification;
 import org.computer.knauss.reqtDiscussion.model.IClassificationFilter;
+import org.computer.knauss.reqtDiscussion.model.clarificationPatterns.BackToDraftPattern;
+import org.computer.knauss.reqtDiscussion.model.clarificationPatterns.DiscordantPattern;
+import org.computer.knauss.reqtDiscussion.model.clarificationPatterns.HappyEndingPattern;
+import org.computer.knauss.reqtDiscussion.model.clarificationPatterns.IndifferentPattern;
+import org.computer.knauss.reqtDiscussion.model.clarificationPatterns.ProcrastinationPattern;
+import org.computer.knauss.reqtDiscussion.model.clarificationPatterns.TextbookPattern;
 import org.computer.knauss.reqtDiscussion.model.machineLearning.ClassifierManager;
 import org.computer.knauss.reqtDiscussion.model.metric.PatternMetric;
 
@@ -73,50 +79,77 @@ public class SimpleDiscussionClassifierEvaluationCmd extends
 
 	private void evaluate(Bucket[] buckets) {
 		// TODO output as tex
-		// TODO sort output according to paper
-		// TODO add true positive, false positive, false negative, and true negative
-		// TODO add recall, precision, specificity, and f-measure
+		// map from PatternMetric results (+1) to order of patterns in the
+		// paper, e.g. PatternMetric returns -1 for "unknown", on index 0
+		// (=-1+1) we refer to index 6, which is the position the unknown values
+		// should have in the paper.
+		// the order should be indifferent, happy-ending, discordant,
+		// back-to-draft, text-book, procrastination, unknown
+		// the order is unknown, Indifferent, Discordant, Procrastination,
+		// BackToDraft, HappyEnding, Textbook
+		int[] ordering = new int[] { 6, 0, 2, 5, 3, 1, 4 };
+
+		int[][] confusionMatrix = createConfusionMatrix(buckets, ordering);
+
+		printConfusionMatrix(ordering, confusionMatrix);
+	}
+
+	private void printConfusionMatrix(int[] ordering, int[][] confusionMatrix) {
+		IConfusionMatrixLayouter layouter = new TabLayouter();
+		System.out.println(layouter.layoutConfusionMatrix(confusionMatrix,
+				ordering, metric));
 		
-		// TODO review and double check results
+		layouter = new ConfigurableLayouter("\t", "\n");
+		System.out.println(layouter.layoutConfusionMatrix(confusionMatrix,
+				ordering, metric));
+		layouter = new ConfigurableLayouter(" & ", "\\tabularnewline \n");
+		System.out.println(layouter.layoutConfusionMatrix(confusionMatrix,
+				ordering, metric));	
 		
+	}
+
+	private int[][] createConfusionMatrix(Bucket[] buckets, int[] ordering) {
 		int[][] confusionMatrix = new int[7][7];
 		System.out.println("ID \t Actual \t Predicted");
 		for (Bucket bucket : buckets) {
 			for (Discussion d : bucket) {
-				this.metric.initDiscussions(new Discussion[] { d });
-				IClassificationFilter.NAME_FILTER.setName(REFERENCE_CLASSIFIER);
-				int reference = this.metric.considerDiscussions(
-						new Discussion[] { d }).intValue();
-				IClassificationFilter.NAME_FILTER.setName(this.classifier
-						.getClass().getSimpleName());
-				int classification = this.metric.considerDiscussions(
-						new Discussion[] { d }).intValue();
+				if (considerDiscussion(d)) {
 
-				// metric result starts at -1 (unknown)
-				confusionMatrix[classification + 1][reference + 1]++;
+					this.metric.initDiscussions(new Discussion[] { d });
+					IClassificationFilter.NAME_FILTER
+							.setName(REFERENCE_CLASSIFIER);
+					int reference = this.metric.considerDiscussions(
+							new Discussion[] { d }).intValue();
+					IClassificationFilter.NAME_FILTER.setName(this.classifier
+							.getClass().getSimpleName());
+					int classification = this.metric.considerDiscussions(
+							new Discussion[] { d }).intValue();
 
-				System.out.println(d.getID() + "\t"
-						+ this.metric.decode(reference) + "\t"
-						+ this.metric.decode(classification));
+					// metric result starts at -1 (unknown)
+					confusionMatrix[ordering[classification + 1]][ordering[reference + 1]]++;
+
+					System.out.println(d.getID() + "\t"
+							+ this.metric.decode(reference) + "\t"
+							+ this.metric.decode(classification));
+				}
 			}
 		}
+		return confusionMatrix;
+	}
 
-		// Print the heading of the confusion matrix
-		System.out.println(this.classifier.getClass().getSimpleName() + "/"
-				+ REFERENCE_CLASSIFIER + "\t" + this.metric.decode(-1) + "\t"
-				+ this.metric.decode(0) + "\t" + this.metric.decode(1) + "\t"
-				+ this.metric.decode(2) + "\t" + this.metric.decode(3) + "\t"
-				+ this.metric.decode(4) + "\t" + this.metric.decode(5) + "\t"
-				+ this.metric.decode(6));
+	private boolean considerDiscussion(Discussion d) {
+		for (DiscussionEvent de : d.getDiscussionEvents())
+			if (de.isClassified())
+				return true;
+		return false;
+	}
 
-		for (int i = 0; i < 7; i++) {
-			String line = this.metric.decode(i - 1) + "\t";
-			for (int j = 0; j < 7; j++) {
-				line += confusionMatrix[i][j] + "\t";
-			}
-
-			System.out.println(line);
+	private int reverse(int index, int[] ordering) {
+		for (int i = 0; i < ordering.length; i++) {
+			if (ordering[i] == index)
+				return i;
 		}
+		return -1;
 	}
 
 	private void classify(Bucket bucket) {
@@ -169,6 +202,157 @@ public class SimpleDiscussionClassifierEvaluationCmd extends
 	private class Bucket extends LinkedList<Discussion> {
 
 		private static final long serialVersionUID = 1L;
+
+	}
+
+	private interface IConfusionMatrixLayouter {
+		String layoutConfusionMatrix(int[][] matrix, int[] ordering,
+				PatternMetric metric);
+	}
+
+	private class TabLayouter implements IConfusionMatrixLayouter {
+
+		@Override
+		public String layoutConfusionMatrix(int[][] matrix, int[] ordering,
+				PatternMetric metric) {
+			// Print the heading of the confusion matrix
+			String line = classifier.getClass().getSimpleName();
+			line += "/" + REFERENCE_CLASSIFIER;
+			for (int i = 0; i < 7; i++) {
+				line += "\t" + metric.decode(reverse(i, ordering) - 1);
+			}
+			line += "\t true positive \t false positive \t false negative \t true negative \t recall \t precision \t f-measure \t specificity";
+
+			int sum = 0;
+			for (int i = 0; i < 7; i++) {
+				for (int j = 0; j < 7; j++) {
+					sum += matrix[i][j];
+				}
+			}
+
+			for (int i = 0; i < 7; i++) {
+				line += "\n" + metric.decode(reverse(i, ordering) - 1) + "\t";
+				int tp = 0, fp = 0, fn = 0, tn = 0;
+				for (int j = 0; j < 7; j++) {
+					line += matrix[i][j] + "\t";
+					if (i == j) {
+						tp = matrix[i][j];
+						for (int n = 0; n < 7; n++) {
+							if (n != j)
+								fn += matrix[n][j];
+						}
+					} else {
+						fp += matrix[i][j];
+					}
+				}
+				tn = sum - (tp + fp + fn);
+				line += tp + "\t" + fp + "\t" + fn + "\t" + tn;
+				// recall
+				double recall = ((double) tp) / (((double) tp) + ((double) fn));
+				line += "\t" + recall;
+				// precision
+				double precision = ((double) tp)
+						/ (((double) tp) + ((double) fp));
+				line += "\t" + precision;
+				// f-measure
+				line += "\t" + (2 * recall * precision) / (recall + precision);
+				// specificity
+				line += "\t" + ((double) tn) / (((double) tn) + ((double) fp));
+			}
+			return line;
+		}
+
+	}
+
+	private class ConfigurableLayouter implements IConfusionMatrixLayouter {
+
+		private String rowSep;
+		private String colSep;
+
+		public ConfigurableLayouter(String colSep, String rowSep) {
+			this.colSep = colSep;
+			this.rowSep = rowSep;
+		}
+
+		@Override
+		public String layoutConfusionMatrix(int[][] matrix, int[] ordering,
+				PatternMetric metric) {
+			// Print the heading of the confusion matrix
+			StringBuffer line = new StringBuffer();
+			line.append(classifier.getClass().getSimpleName());
+			line.append("/");
+			line.append(REFERENCE_CLASSIFIER);
+			for (int i = 0; i < 7; i++) {
+				line.append(colSep);
+				line.append(metric.decode(reverse(i, ordering) - 1));
+			}
+			line.append(colSep);
+			line.append("true positive");
+			line.append(colSep);
+			line.append("false positive");
+			line.append(colSep);
+			line.append("false negative");
+			line.append(colSep);
+			line.append("true negative");
+			line.append(colSep);
+			line.append("recall");
+			line.append(colSep);
+			line.append("precision");
+			line.append(colSep);
+			line.append("f-measure");
+			line.append(colSep);
+			line.append("specificity");
+
+			int sum = 0;
+			for (int i = 0; i < 7; i++) {
+				for (int j = 0; j < 7; j++) {
+					sum += matrix[i][j];
+				}
+			}
+
+			for (int i = 0; i < 7; i++) {
+				line.append(rowSep);
+				line.append(metric.decode(reverse(i, ordering) - 1));
+				line.append(colSep);
+				int tp = 0, fp = 0, fn = 0, tn = 0;
+				for (int j = 0; j < 7; j++) {
+					line.append(matrix[i][j]);
+					line.append(colSep);
+					if (i == j) {
+						tp = matrix[i][j];
+						for (int n = 0; n < 7; n++) {
+							if (n != j)
+								fn += matrix[n][j];
+						}
+					} else {
+						fp += matrix[i][j];
+					}
+				}
+				tn = sum - (tp + fp + fn);
+				line.append(tp);
+				line.append(colSep);
+				line.append(fp);
+				line.append(colSep);
+				line.append(fn);
+				line.append(colSep);
+				line.append(tn);
+				// recall
+				double recall = ((double) tp) / (((double) tp) + ((double) fn));
+				line.append(colSep);
+				line.append(recall);
+				// precision
+				double precision = ((double) tp)
+						/ (((double) tp) + ((double) fp));
+				line.append(colSep);
+				line.append(precision);
+				// f-measure
+				line.append(colSep);
+				line.append((2 * recall * precision) / (recall + precision));
+				// specificity
+				line.append(((double) tn) / (((double) tn) + ((double) fp)));
+			}
+			return line.toString();
+		}
 
 	}
 
