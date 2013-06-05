01facf3ed117a10b43b427b49c931256d03d8dbe
Eric Knauss <eric.knauss@googlemail.com>
2013-06-05 16:06:46 -0700

    Fix #70 - Now I have one reasonable spot for this code.

--- RequirementsDiscussionAnalyzer/src/main/java/org/computer/knauss/reqtDiscussion/ui/ctrl/HighlightRelatedDiscussions.java
+++ RequirementsDiscussionAnalyzer/src/main/java/org/computer/knauss/reqtDiscussion/ui/ctrl/HighlightRelatedDiscussions.java
@@ -5,7 +5,12 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.URL;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 import java.util.Vector;
 
 import org.computer.knauss.reqtDiscussion.Util;
@@ -15,6 +20,8 @@ import org.computer.knauss.reqtDiscussion.ui.uiModel.DiscussionTableModel;
 public class HighlightRelatedDiscussions {
 
 	private List<int[]> ids = new Vector<int[]>();
+	private Set<Integer> visited = new HashSet<Integer>();
+	private List<Discussion[]> complexDiscussions;
 
 	public HighlightRelatedDiscussions() throws IOException {
 		URL url = getClass().getResource("/related-work-items.txt");
@@ -71,6 +78,44 @@ public class HighlightRelatedDiscussions {
 
 			}
 		}
+	}
+
+	/**
+	 * Computes a list of all discussions, where complex discussions are
+	 * aggregated. Each discussion is represented by a Discussion-Array, this
+	 * array has length = 1 for non-complex discussions.
+	 * 
+	 * @return
+	 */
+	public List<Discussion[]> getAllAggregatedDiscussions(
+			Discussion[] discussions) {
+		if (this.visited.size() == discussions.length && complexDiscussions != null) 
+			return this.complexDiscussions;
+		
+		this.visited.clear();
+		
+		Map<Integer, List<Discussion>> results = new HashMap<Integer, List<Discussion>>();
+		
+		for (Discussion d : discussions) {
+			if (!this.visited.contains(d.getID())) {
+				int[] related = getRelatedDiscussionIDs(d.getID());
+				List<Discussion> relDisc = results.get(related[0]);
+				if (relDisc == null){
+					relDisc = new LinkedList<Discussion>();
+					results.put(related[0], relDisc);
+				}
+				relDisc.add(d);
+				this.visited.add(d.getID());
+			}
+		}
+		
+
+		// Compute the result
+		complexDiscussions = new LinkedList<Discussion[]>();
+		for (List<Discussion> l : results.values()){
+			complexDiscussions.add(l.toArray(new Discussion[0]));
+		}
+		return complexDiscussions;
 	}
 
 }
--- RequirementsDiscussionAnalyzer/src/test/java/org/computer/knauss/reqtDiscussion/ui/ctrl/HighlightRelatedDiscussionsTest.java
+++ RequirementsDiscussionAnalyzer/src/test/java/org/computer/knauss/reqtDiscussion/ui/ctrl/HighlightRelatedDiscussionsTest.java
@@ -2,6 +2,10 @@ package org.computer.knauss.reqtDiscussion.ui.ctrl;
 
 import static org.junit.Assert.assertEquals;
 
+import java.util.List;
+
+import org.computer.knauss.reqtDiscussion.model.Discussion;
+import org.computer.knauss.reqtDiscussion.model.DiscussionFactory;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -33,13 +37,42 @@ public class HighlightRelatedDiscussionsTest {
 		assertEquals(40774, wiIDs[1]);
 		assertEquals(40775, wiIDs[2]);
 	}
-	
+
 	@Test
-	public void testNonExisting() { 
+	public void testNonExisting() {
 		int[] wiIDs = this.hrw.getRelatedDiscussionIDs(0123);
-		
+
 		assertEquals(1, wiIDs.length);
 		assertEquals(0123, wiIDs[0]);
+	}
+
+	@Test
+	public void testGetAllAggregatedDiscussions() {
+		Discussion[] discussions = new Discussion[4];
+		discussions[0] = DiscussionFactory.getInstance().getDiscussion(0123);
+		discussions[1] = DiscussionFactory.getInstance().getDiscussion(13764);
+		discussions[2] = DiscussionFactory.getInstance().getDiscussion(40774);
+		discussions[3] = DiscussionFactory.getInstance().getDiscussion(40775);
+
+		List<Discussion[]> result = this.hrw
+				.getAllAggregatedDiscussions(discussions);
+		assertEquals(2, result.size());
+		assertEquals(1, result.get(1).length);
+		assertEquals(3, result.get(0).length);
+	}
+
+	@Test
+	public void testGetAllAggregatedDiscussionsIncomplete() {
+		Discussion[] discussions = new Discussion[3];
+		discussions[0] = DiscussionFactory.getInstance().getDiscussion(0123);
+		discussions[1] = DiscussionFactory.getInstance().getDiscussion(13764);
+		discussions[2] = DiscussionFactory.getInstance().getDiscussion(40774);
+
+		List<Discussion[]> result = this.hrw
+				.getAllAggregatedDiscussions(discussions);
+		assertEquals(2, result.size());
+		assertEquals(1, result.get(1).length);
+		assertEquals(2, result.get(0).length);
 	}
 
 }
