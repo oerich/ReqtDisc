c2fa1822f190c7b580399420655c43abe662e9ff
Eric Knauss <eric.knauss@googlemail.com>
2013-06-11 16:14:03 -0700

    Change interface of DiscussionEventDAO #55: Change scheme of workitemcomments of old data https://github.com/oerich/ReqtDisc/issues/issue/55

--- RequirementsDiscussionAnalyzer/src/main/java/org/computer/knauss/reqtDiscussion/io/IDiscussionEventClassificationDAO.java
+++ RequirementsDiscussionAnalyzer/src/main/java/org/computer/knauss/reqtDiscussion/io/IDiscussionEventClassificationDAO.java
@@ -1,11 +1,12 @@
 package org.computer.knauss.reqtDiscussion.io;
 
+import org.computer.knauss.reqtDiscussion.model.DiscussionEvent;
 import org.computer.knauss.reqtDiscussion.model.DiscussionEventClassification;
 
 public interface IDiscussionEventClassificationDAO {
 
 	public DiscussionEventClassification[] getClassificationsForDiscussionEvent(
-			int discEventID) throws DAOException;
+			DiscussionEvent de) throws DAOException;
 
 	public void storeDiscussionEventClassification(
 			DiscussionEventClassification classification) throws DAOException;
